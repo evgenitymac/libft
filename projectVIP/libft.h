3d08a6100d9ca7343c497c5b956384eebc5ac6f5
Marcelina Heidenreich <maheiden@e3r8p7.21-school.ru>
2018-11-23 19:08:40 +0300

    hello

---
+++ projectVIP/libft.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maheiden <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/23 16:29:08 by maheiden          #+#    #+#             */
+/*   Updated: 2018/11/23 18:42:08 by maheiden         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef local
+# define local
+
+#include <string.h>
+#include <stdlib.h>
+
+void	ft_bzero(void *s, size_t n);
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memset(void *b, int c, size_t len);
+size_t	ft_stren(const char *s);
+char	*strdup(const char *s1);
+
+#endif
